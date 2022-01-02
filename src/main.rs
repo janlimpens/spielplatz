1ae5d1e1908b7b36b695ec5774a8264a4211ce5e
Jan Limpens <jan@limpens.com>
2022-01-02 17:28:17 +0100

    String -> $str

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 #![feature(entry_insert)]
 #[allow(dead_code)]
 use regex::Regex;
-use std::{collections::{HashMap, HashSet}, ops::ControlFlow};
+use std::{
+    collections::{HashMap, HashSet}
+};
 mod r#async;
 fn main() {
     Bayes::new();
@@ -11,6 +13,7 @@ fn main() {
 struct Bayes {
     matrix: HashMap<Categorization, u32>,
     ignore_words: HashSet<String>,
+    tokenizer: Regex,
 }
 
 #[derive(PartialEq, Hash, Eq, Debug, Clone)]
@@ -20,17 +23,19 @@ struct Categorization {
 }
 
 impl Bayes {
+
     fn new() -> Self {
         let mut bayes = Bayes {
             matrix: HashMap::new(),
             ignore_words: HashSet::new(),
+            tokenizer: Regex::new(r#"[\w'-]+"#).unwrap()
         };
         bayes.load_ignore_words();
         bayes
     }
 
     fn load_ignore_words(&mut self) {
-        for word in "be i am are is was were a an this the and or so much not no nor"
+        for word in "be i am are is was were a an this the and or so much not no nor do don't"
             .to_string()
             .split(" ")
         {
@@ -38,34 +43,36 @@ impl Bayes {
         }
     }
 
-    fn learn(&mut self, text: String, bucket: String) {
+    /// chops a string into word like pieces
+    pub fn tokenize(&self, text: &str) -> Vec<String> {
+        let text = text.to_lowercase();
+        self.tokenizer
+            .find_iter(&text)
+            .map(|m| m.as_str().to_string())
+            .collect::<Vec<String>>()
+    }
+
+    /// manually categorize a text
+    pub fn learn(&mut self, text: &str, bucket: &str) {
         if text == String::default() || bucket == String::default() {
             return;
         }
-        let bucket = bucket.to_lowercase();
-        let text = text.to_lowercase();
-        let regex = Regex::new("([a-z]*)").unwrap();
-        let matches = regex.find_iter(&text);
-        for word in matches {
-            let word = word.as_str().to_string();
-            let bucket = bucket.clone();
-            self.add_word_to_bucket(word, bucket);
+        let bucket = &bucket.to_lowercase();
+        for word in self.tokenize(text) {
+            self.add_word_to_bucket(&word, bucket);
         }
     }
 
-    fn add_word_to_bucket(&mut self, word: String, bucket: String) {
-        if word == String::default() || self.ignore_words.contains(&word) {
+    pub fn add_word_to_bucket(&mut self, word: &str, bucket: &str) {
+        if word == String::default() || self.ignore_words.contains(word) {
             return;
         }
-        let key = Categorization {
-            word,
-            bucket,
-        };
+        let key = Categorization { word: word.to_string(), bucket: bucket.to_string() };
         let entry = self.matrix.entry(key).or_insert(0);
         *entry += 1;
     }
 
-    fn get_scores(&self, word: String) -> Vec<(String, u32)> {
+    fn get_scores(&self, word: &str) -> Vec<(String, u32)> {
         let mut x: Vec<(String, u32)> = self
             .matrix
             .keys()
@@ -76,39 +83,39 @@ impl Bayes {
                 (bucket, score)
             })
             .collect();
-        x.sort_by(|a, b| b.1.cmp(&a.1));
+        x.sort_by(|(_, a), (_, b)| b.cmp(&a));
         x
     }
 
-    fn display(&self) {
+    fn display_matrix(&self) {
         for (word, score) in &self.matrix {
             println!("Key: {}/{} Val:{}", word.word, word.bucket, score);
         }
     }
 
-    fn guess_bucket(&self, text: String) -> String {
+    fn guess_bucket(&self, text: &str) -> Vec<String> {
         if text == String::default() {
-            return text;
+            return vec![];
         }
-        let regex = Regex::new("([a-z]*)").unwrap();
-        let matches = regex.find_iter(&text);
         let mut bucket_score: HashMap<String, u32> = HashMap::new();
-        for token in matches {
-            let scores = self.get_scores(token.as_str().to_string());
+        for token in self.tokenize(text) {
+            let scores = self.get_scores(&token);
             for (bucket, score) in scores {
                 let entry = bucket_score.entry(bucket).or_default();
                 *entry += score;
             }
         }
         let mut result: Vec<(&String, &u32)> = bucket_score.iter().collect();
-        result.sort_by(|a, b| b.cmp(&a));
+        result.sort_by(|(_, score_a), (_, score_b)| score_b.cmp(&score_a));
         if result.is_empty() {
-            return String::default();
+            return Vec::new();
         }
-        for r in &result {
-            println!("{} with score {}", r.0, r.1);
-        }
-        result[0].0.to_owned()
+        let (_, &highscore) = result[0];
+        return result
+            .iter()
+            .filter(|(_, &score)| score == highscore)
+            .map(|(bucket, _)| bucket.to_string())
+            .collect::<Vec<String>>();
     }
 }
 #[cfg(test)]
@@ -131,39 +138,49 @@ mod tests {
             ("In the filthy store they sell magazines", "magazine"),
         ];
         let mut bayes = Bayes::new();
-        learning_body.iter().for_each(|item| {
-            bayes.learn(item.0.to_string(), item.1.to_string());
+        learning_body.iter().for_each(|(text, bucket)| {
+            bayes.learn(text, bucket);
         });
         bayes
     }
 
     #[test]
+    fn test_tokenizer() {
+        let bayes = Bayes::new();
+        let matches = bayes.tokenize("some words, some with áccents
+        (and hyphen-separated) 2021  !");
+        assert_eq!(matches.into_iter().count(), 8);
+    }
+
+    #[test]
     fn test_learn() {
         let bayes = get_taught_bayes();
-        bayes.display();
-        assert!(bayes.get_scores("i".to_string()).is_empty());
-        assert_eq!(bayes.get_scores("reads".to_string()).first().unwrap().1, 2);
+        bayes.display_matrix();
+        assert!(bayes.get_scores("i").is_empty());
+        let (bucket, score) = &bayes.get_scores("reads")[0];
+        assert_eq!(score, &2);
+        assert_eq!(bucket, "book");
     }
 
     #[test]
     fn get_scores_returns_results_in_good_order() {
         let bayes = get_taught_bayes();
-        let book_scores = bayes.get_scores("reads".to_string());
-        for score in &book_scores {
-            println!("Bucket: '{}' Score: {}", score.0, score.1)
+        let scores_for_reads = bayes.get_scores("reads");
+        for (bucket, score) in &scores_for_reads {
+            println!("Bucket: '{bucket}' Score: {score}")
         }
-        assert_eq!(book_scores.first().unwrap().1.to_owned(), 2);
-        assert_eq!(book_scores[1].1.to_owned(), 1);
+        assert_eq!(scores_for_reads[0].1, 2u32);
+        assert_eq!(scores_for_reads[1].1, 1u32);
     }
 
     #[test]
     fn get_a_good_guess() {
         let bayes = get_taught_bayes();
-        let assumed_bucket =
-            bayes.guess_bucket("Miss so and so visits the classical library".to_string());
-        assert_eq!(assumed_bucket, "book".to_string());
-        let assumed_bucket =
-            bayes.guess_bucket("He goes to the store and reads filthy publications".to_string());
-        assert_eq!(assumed_bucket, "magazine".to_string());
+        let assumed_buckets =
+            bayes.guess_bucket("Miss so and so visits the classical library");
+        assert_eq!(assumed_buckets[0], "book".to_string());
+        let assumed_buckets =
+            bayes.guess_bucket("He goes to the store and reads filthy publications");
+        assert_eq!(assumed_buckets[0], "magazine".to_string());
     }
 }
